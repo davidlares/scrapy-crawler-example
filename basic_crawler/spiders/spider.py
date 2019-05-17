4070146591116b4ef7164f058fda83a7dcc5621b
David E Lares S <david.e.lares@gmail.com>
2019-05-17 14:19:13 -0500

    Adding crawling titles power to other pages

--- basic_crawler/spiders/spider.py
+++ basic_crawler/spiders/spider.py
@@ -2,6 +2,7 @@ from scrapy.spiders import BaseSpider
 from scrapy.selector import Selector
 from basic_crawler.items import BasicCrawlerItem
 from scrapy.http import Request
+import re
 
 class MySpider(BaseSpider):
 
@@ -19,3 +20,19 @@ class MySpider(BaseSpider):
             book = BasicCrawlerItem() # creating Item (items.py)
             book["title"] = title # assignation
             yield book
+
+        # checking links
+        visited_links=[]
+        links = hxs.xpath('//a/@href').extract()
+        # regex for http and https
+        link_validator= re.compile("^(?:http|https):\/\/(?:[\w\.\-\+]+:{0,1}[\w\.\-\+]*@)?(?:[a-z0-9\-\.]+)(?::[0-9]+)?(?:\/|\/(?:[\w#!:\.\?\+=&amp;%@!\-\/\(\)]+)|\?(?:[\w#!:\.\?\+=&amp;%@!\-\/\(\)]+))?$")
+
+        # iterating links
+        for link in links:
+        	if link_validator.match(link) and not link in visited_links: # absolute URL and not visited
+        		visited_links.append(link)
+        		yield Request(link, self.parse)
+        	else:
+        		full_url=response.urljoin(link)
+        		visited_links.append(full_url) # creating an absolute URL
+        		yield Request(full_url, self.parse)
